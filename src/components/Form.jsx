017dcb6971930961d6d7b6812e25befbd76f5a5e
jeffjuann <jeffjuann@gmail.com>
2023-06-28 22:40:56 +0700

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,23 +6,13 @@ const InputForm = ({ handleTextLink }) =>
 {
     const [text, setText] = useState('');
 
-    const handleOnChange = (e) =>
-    {
-        setText(e.target.value);
-    }
-
-    const submitBtn = () =>
-    {
-        handleTextLink(text);
-    }
-
     return (
         <div className="form">
             <div className="textinput">
                 <label htmlFor="text-input">Text/Link :</label>
-                <input type="text" onChange={handleOnChange} placeholder="input text or link here ..."/>
+                <input type="text" onChange={(e) => setText(e.target.value)} placeholder="input text or link here ..."/>
             </div>
-            <button onClick={submitBtn}>Generate</button>
+            <button onClick={() => handleTextLink(text)}>Generate</button>
         </div>
     )
 }
